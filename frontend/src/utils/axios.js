3b02fd752e7e36282e0b2706b40c445db0f32c2f
M Harsha Vardhan Reddy <harshavardhanreddymadhira@gmail.com>
2025-06-27 15:13:50 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL:"https://admindashboard-jobportal.onrender.com"
+    baseURL:"https://admindashboard-jobportal.onrender.com/api"
 })
 
 export default axiosInstance;
